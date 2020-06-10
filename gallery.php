10efaf6fe11292183d47ee88770d9808d607fa15
lirta <indalirta@gmail.com>
2020-06-10 15:17:48 +0700

    fron end

---
+++ gallery.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<?php include 'header.php'; ?>
+<main>
+    <!--? Hero Start -->
+    <div class="slider-area2">
+        <div class="slider-height2 hero-overly d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="hero-cap hero-cap2 text-center pt-80">
+                            <h2>Our Portfolio</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Hero End -->
+    <!--? our info Start -->
+    <div class="our-info-area pt-170 pb-100 section-bg" data-background="assets/img/gallery/section_bg02.jpg">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-info mb-60">
+                        <div class="info-caption">
+                            <h4>Clean and Services</h4>
+                            <p>For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-info mb-60">
+                        <div class="info-caption">
+                            <h4>Clean and Modern</h4>
+                            <p>For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-info mb-60">
+                        <div class="info-caption">
+                            <h4>Clean and Modern</h4>
+                            <p>For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Our Info start -->
+    <!--? Services Ara Start -->
+    <div class="services-area section-padding3">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="cl-xl-7 col-lg-8 col-md-10">
+                    <!-- Section Tittle -->
+                    <div class="section-tittle text-center mb-70">
+                        <span>Our Professional Services</span>
+                        <h2>Best Interitor Services</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-10">
+                    <div class="single-services mb-200">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/services1.png" alt="">
+                        </div>
+                        <div class="services-caption">
+                            <h3><a href="#">Lighting</a></h3>
+                            <p class="pera1">For each project we establish </p>
+                            <p class="pera2">For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10">
+                    <div class="single-services mb-200">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/services2.png" alt="">
+                        </div>
+                        <div class="services-caption">
+                            <h3><a href="#">Interior Design</a></h3>
+                            <p class="pera1">For each project we establish </p>
+                            <p class="pera2">For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10">
+                    <div class="single-services mb-200">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/services3.png" alt="">
+                        </div>
+                        <div class="services-caption">
+                            <h3><a href="#">Office Decoretion</a></h3>
+                            <p class="pera1">For each project we establish </p>
+                            <p class="pera2">For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10">
+                    <div class="single-services mb-200">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/services4.png" alt="">
+                        </div>
+                        <div class="services-caption">
+                            <h3><a href="#">Lighting</a></h3>
+                            <p class="pera1">For each project we establish </p>
+                            <p class="pera2">For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10">
+                    <div class="single-services mb-200">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/services5.png" alt="">
+                        </div>
+                        <div class="services-caption">
+                            <h3><a href="#">Interior Design</a></h3>
+                            <p class="pera1">For each project we establish </p>
+                            <p class="pera2">For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10">
+                    <div class="single-services mb-200">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/services6.png" alt="">
+                        </div>
+                        <div class="services-caption">
+                            <h3><a href="#">Office Decoretion</a></h3>
+                            <p class="pera1">For each project we establish </p>
+                            <p class="pera2">For each project we establish relationships with partners who we know will help us. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Services Ara End -->
+    <!-- Brand Area Start -->
+    <div class="brand-area pt-120 pb-120">
+        <div class="container">
+            <div class="brand-active brand-border pt-50 pb-40">
+                <div class="single-brand">
+                    <img src="assets/img/gallery/brand1.png" alt="">
+                </div>
+                <div class="single-brand">
+                    <img src="assets/img/gallery/brand2.png" alt="">
+                </div>
+                <div class="single-brand">
+                    <img src="assets/img/gallery/brand3.png" alt="">
+                </div>
+                <div class="single-brand">
+                    <img src="assets/img/gallery/brand4.png" alt="">
+                </div>
+                <div class="single-brand">
+                    <img src="assets/img/gallery/brand2.png" alt="">
+                </div>
+                <div class="single-brand">
+                    <img src="assets/img/gallery/brand5.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Brand Area End -->
+    <!-- Want To work -->
+    <section class="wantToWork-area w-padding2">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-xl-8 col-lg-8 col-md-8">
+                    <div class="wantToWork-caption wantToWork-caption2">
+                        <h2>Are you Searching For a First-Class Consultant?</h2>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-3">
+                    <a href="#" class="btn btn-black f-right">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Want To work End -->
+</main>
+<?php include 'footer.php'; ?>
+<!-- Scroll Up -->
+<div id="back-top">
+    <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+</div>
+
+<!-- JS here -->
+<!-- All JS Custom Plugins Link Here here -->
+<script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+<!-- Jquery, Popper, Bootstrap -->
+<script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="./assets/js/popper.min.js"></script>
+<script src="./assets/js/bootstrap.min.js"></script>
+<!-- Jquery Mobile Menu -->
+<script src="./assets/js/jquery.slicknav.min.js"></script>
+
+<!-- Jquery Slick , Owl-Carousel Plugins -->
+<script src="./assets/js/owl.carousel.min.js"></script>
+<script src="./assets/js/slick.min.js"></script>
+<!-- One Page, Animated-HeadLin -->
+<script src="./assets/js/wow.min.js"></script>
+<script src="./assets/js/animated.headline.js"></script>
+<script src="./assets/js/jquery.magnific-popup.js"></script>
+
+<!-- Nice-select, sticky -->
+<script src="./assets/js/jquery.nice-select.min.js"></script>
+<script src="./assets/js/jquery.sticky.js"></script>
+
+<!-- contact js -->
+<script src="./assets/js/contact.js"></script>
+<script src="./assets/js/jquery.form.js"></script>
+<script src="./assets/js/jquery.validate.min.js"></script>
+<script src="./assets/js/mail-script.js"></script>
+<script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+
+<!-- Jquery Plugins, main Jquery -->
+<script src="./assets/js/plugins.js"></script>
+<script src="./assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,74 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>CV. KAYANA CITRA GEMILANG</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="body-bg">
+    <!--? Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/loder.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <header>
+        <!-- Header Start -->
+        <div class="header-area header-transparent">
+            <div class="main-header header-sticky">
+                <div class="container-fluid">
+                    <div class="menu-wrapper d-flex align-items-center justify-content-between">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.php">CV. KAYANA CITRA GEMILANG</a>
+                        </div>
+                        <!-- Main-menu -->
+                        <div class="main-menu f-right d-none d-lg-block">
+                            <nav>
+                                <ul id="navigation">
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="about.php">About</a></li>
+                                    <li><a href="katalog.php">Katalog</a></li>
+                                    <li><a href="gallery.php">Gallery</a></li>
+                                    <li><a href="#">Log in</a>
+                                        <ul class="submenu">
+                                            <li><a href="system/login-konsumen.php">Konsumen</a></li>
+                                            <li><a href="system/login.php">Kariawan</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <!-- Header-btn -->
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+    </header>
